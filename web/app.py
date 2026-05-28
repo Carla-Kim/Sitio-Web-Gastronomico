14a0b4ef56f053ffb88f8d51c3f3be593af8ae86
Nicolas West <nico.westt@gmail.com>
2026-05-27 22:24:16 -0300

    feature/app,py-dashboard.html

--- web/app.py
+++ web/app.py
@@ -19,10 +19,101 @@ def login():
     # hay que agregar lógica para manejar el inicio de sesión, validación de usuarios, etc. antes de mostrar esta página
     return render_template('login.html')
 
-@app.route('/admin')
-def admin_panel():
-    # hay que agregar lógica para verificar si el usuario es admin o un usuario válido antes de mostrar esta página
-    return render_template('admin.html')
+@app.route('/dashboard')
+def dashboard_landing():
+    return render_template('dashboard-inicio.html')
+
+@app.route('/dashboard/login')
+def dashboard_login():
+    return render_template('dashboard-login.html')
+
+@app.route('/dashboard/reservas')
+def dashboard_reservas():
+    return render_template('dashboard-reservas.html')
+
+@app.route('/dashboard/reseñas')
+def dashboard_reseñas():
+    lista_reseñas = [
+        {
+            "id": 3,
+            "id_reserva": 4,
+            "nombre": "Nicolás",
+            "apellido": "West",
+            "comentario": "Muy bueno todo", 
+            "ambiente": 4,
+            "servicio": 3,
+            "comida": 4,
+            "fecha": "25/05/2026"
+        },   
+        {
+            "id": 2,
+            "id_reserva": 5,
+            "nombre": "Agustín",
+            "apellido": "West",
+            "comentario": "Muy bueno todo", 
+            "ambiente": 2,
+            "servicio": 1,
+            "comida": 5,
+            "fecha": "26/05/2026"
+        }   
+    ] 
+    return render_template('dashboard-reseñas.html', reseñas=lista_reseñas)
+
+@app.route('/dashboard/menu')
+def dashboard_menu():
+    # logica de verificacion de si el usuario es admin o no.
+    # logica para usar los datos de la base de datos.
+    lista_productos = [
+        {
+            "id": 1, 
+            "nombre": "Milanesa con Papas Fritas", 
+            "categoria": "Platos Principales", 
+            "precio": 4500, 
+            "descripcion": "Milanesa de carne vacuna acompañada de papas fritas bastón."
+        },
+        {
+            "id": 2, 
+            "nombre": "Empanada de Carne", 
+            "categoria": "Entradas", 
+            "precio": 800, 
+            "descripcion": "Empanada criolla frita, cortada a cuchillo."
+        },
+        {
+            "id": 3, 
+            "nombre": "Volcán de Chocolate", 
+            "categoria": "Postres", 
+            "precio": 2200, 
+            "descripcion": "Bizcocho tibio con corazón de chocolate fundido y helado."
+        }   
+    ]
+    lista_categorias = [
+        {"id": 1, "nombre": "Platos principales"},
+        {"id": 2, "nombre": "Bebidas"},
+        {"id": 3, "nombre": "Entradas"}
+    ]
+    return render_template('dashboard-menu.html', productos=lista_productos, categorias=lista_categorias)
+
+@app.route('/dashboard/usuarios')
+def dashboard_usuarios():
+    lista_usuarios = [
+        {
+            "id": 3,
+            "usuario": "Pepe",
+            "nombre": "Nicolás",
+            "apellido": "West",
+            "email": "nwest@fi.uba.ar", 
+            "rol": "Admin",
+        },
+        {
+            "id": 2,
+            "usuario": "Pepe2",
+            "nombre": "Agustín",
+            "apellido": "West",
+            "email": "nwest@fi.uba.ar", 
+            "rol": "Usuario",
+        }      
+    ] 
+    return render_template('dashboard-usuarios.html', usuarios=lista_usuarios)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
