f8df1271c45abef689ff4c41d32600150aeb8afb
Nicolas West <nico.westt@gmail.com>
2026-06-02 16:19:33 -0300

    feat: app.py and error handling

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 import requests
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -28,17 +28,43 @@ def dashboard_landing():
 def dashboard_login():
     return render_template('dashboard-login.html')
 
-@app.route('/dashboard/reservas')
+@app.route('/dashboard/reservas', methods=['GET', 'POST'])
 def dashboard_reservas():
-    return render_template('dashboard-reservas.html')
+    if request.method == 'POST':
+       reserva_a_eliminar = request.form.get('id_reserva')
+       if not reserva_a_eliminar:
+           flash("No se pudo completar la acción: ID inexistente")
+           return redirect(url_for('dashboard_reservas'))
+       try:
+            url_api = f'http://localhost:5000/reservas/{reserva_a_eliminar}'
+            response = requests.delete(url_api, timeout=5)
+            response.raise_for_status()
+            return redirect(url_for('dashboard_reservas'))
+       except requests.exceptions.RequestException as e:
+            print(f"Error al eliminar en API: {e}")
+            return render_template('error.html'), 500
+    else:
+       try:
+           url_api = 'http://localhost:5000/reservas'
+           response = requests.get(url_api, timeout=5)
+           response.raise_for_status()
+    
+           lista_reservas = response.json()
+           return render_template('dashboard-reservas.html', reservas=lista_reservas)
+       except requests.exceptions.RequestException as e:
+           print(f"Error crítico al conectar con la API: {e}")
+           return render_template('error-conexion.html'),500 
 
 @app.route('/dashboard/reseñas', methods=['GET', 'POST'])
 def dashboard_reseñas():
     if request.method == 'POST':
        reseña_a_eliminar = request.form.get('id_reseña')
+       if not reseña_a_eliminar:
+           flash("No se pudo completar la acción: ID inexistente")
+           return redirect(url_for('dashboard_reseñas'))
        try:
-            url_api = f'http://localhost:5000/reseñas/eliminar/{reseña_a_eliminar}'
-            response = requests.post(url_api, timeout=5)
+            url_api = f'http://localhost:5000/resenas/{reseña_a_eliminar}'
+            response = requests.delete(url_api, timeout=5)
             response.raise_for_status()
             return redirect(url_for('dashboard_reseñas'))
        except requests.exceptions.RequestException as e:
@@ -46,7 +72,7 @@ def dashboard_reseñas():
             return render_template('error-conexion.html'), 500
     else:
        try:
-           url_api = 'http://localhost:5000/reseñas'
+           url_api = 'http://localhost:5000/resenas'
            response = requests.get(url_api, timeout=5)
            response.raise_for_status()
     
@@ -56,61 +82,67 @@ def dashboard_reseñas():
            print(f"Error crítico al conectar con la API: {e}")
            return render_template('error-conexion.html'),500 
 
-@app.route('/dashboard/menu')
+@app.route('/dashboard/menu', methods=['GET', 'POST'])
 def dashboard_menu():
-    # logica de verificacion de si el usuario es admin o no.
-    # logica para usar los datos de la base de datos.
-    lista_productos = [
-        {
-            "id": 1, 
-            "nombre": "Milanesa con Papas Fritas", 
-            "categoria": "Platos Principales", 
-            "precio": 4500, 
-            "descripcion": "Milanesa de carne vacuna acompañada de papas fritas bastón."
-        },
-        {
-            "id": 2, 
-            "nombre": "Empanada de Carne", 
-            "categoria": "Entradas", 
-            "precio": 800, 
-            "descripcion": "Empanada criolla frita, cortada a cuchillo."
-        },
-        {
-            "id": 3, 
-            "nombre": "Volcán de Chocolate", 
-            "categoria": "Postres", 
-            "precio": 2200, 
-            "descripcion": "Bizcocho tibio con corazón de chocolate fundido y helado."
-        }   
-    ]
-    lista_categorias = [
-        {"id": 1, "nombre": "Platos principales"},
-        {"id": 2, "nombre": "Bebidas"},
-        {"id": 3, "nombre": "Entradas"}
-    ]
-    return render_template('dashboard-menu.html', productos=lista_productos, categorias=lista_categorias)
+    if request.method == 'POST':
+       producto_a_eliminar = request.form.get('id_producto')
+       if not producto_a_eliminar:
+           flash("No se pudo completar la acción: ID inexistente")
+           return redirect(url_for('dashboard_menu'))
+       try:
+            url_api = f'http://localhost:5000/productos/{producto_a_eliminar}'
+            response = requests.delete(url_api, timeout=5)
+            response.raise_for_status()
+            return redirect(url_for('dashboard_menu'))
+       except requests.exceptions.RequestException as e:
+            print(f"Error al eliminar en API: {e}")
+            return render_template('error-conexion.html'), 500
+    else:
+       try:
+           url_api_categorias = 'http://localhost:5000/categorias'
+           url_api_productos = 'http://localhost:5000/productos'
+           response_categorias = requests.get(url_api_categorias, timeout=5)
+           response_categorias.raise_for_status()
+           response_productos = requests.get(url_api_productos, timeout=5)
+           response_productos.raise_for_status()
 
-@app.route('/dashboard/usuarios')
+           lista_categorias = response_categorias.json()
+           lista_productos = response_productos.json()
+           return render_template('dashboard-menu.html', productos=lista_productos, categorias=lista_categorias)
+       except requests.exceptions.RequestException as e:
+           print(f"Error crítico al conectar con la API: {e}")
+           return render_template('error-conexion.html'),500
+
+@app.route('/dashboard/usuarios', methods=['GET', 'POST'])
 def dashboard_usuarios():
-    lista_usuarios = [
-        {
-            "id": 3,
-            "usuario": "Pepe",
-            "nombre": "Nicolás",
-            "apellido": "West",
-            "email": "nwest@fi.uba.ar", 
-            "rol": "Admin",
-        },
-        {
-            "id": 2,
-            "usuario": "Pepe2",
-            "nombre": "Agustín",
-            "apellido": "West",
-            "email": "nwest@fi.uba.ar", 
-            "rol": "Usuario",
-        }      
-    ] 
-    return render_template('dashboard-usuarios.html', usuarios=lista_usuarios)
+    if request.method == 'POST':
+       usuario_a_eliminar = request.form.get('id_usuario')
+       if not usuario_a_eliminar:
+           flash("No se pudo completar la acción: ID inexistente")
+           return redirect(url_for('dashboard_usuarios'))
+       try:
+            url_api = f'http://localhost:5000/usuarios/{usuario_a_eliminar}'
+            response = requests.delete(url_api, timeout=5)
+            response.raise_for_status()
+            return redirect(url_for('dashboard_usuarios'))
+       except requests.exceptions.RequestException as e:
+            print(f"Error al eliminar en API: {e}")
+            return render_template('error-conexion.html'), 500
+    else:
+       try:
+           url_api = 'http://localhost:5000/usuarios'
+           response = requests.get(url_api, timeout=5)
+           response.raise_for_status()
+    
+           lista_usuarios = response.json()
+           return render_template('dashboard-usuarios.html', usuarios=lista_usuarios)
+       except requests.exceptions.RequestException as e:
+           print(f"Error crítico al conectar con la API: {e}")
+           return render_template('error-conexion.html'),500 
+       
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error-not-found.html'),404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
