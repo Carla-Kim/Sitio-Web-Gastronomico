05bb831031dec69912926a16df1a85b229305dd9
KevinLaRocca <132600372+KevinLaRocca@users.noreply.github.com>
2026-06-03 03:18:07 -0300

    fix: arreglo en services basado en el arreglo de database.

--- api/services/resenas.py
+++ api/services/resenas.py
@@ -11,6 +11,8 @@ def listar_resenas(base_url, limit, offset):
         resenas_formateadas = [{
                 "resena_id": r["resena_id"],
                 "reserva_id": r["reserva_id"],
+                "nombre_usuario": r["nombre_usuario"],
+                "comentario": r["comentario"],
                 "puntuacion_ambiente": r["puntuacion_ambiente"],
                 "puntuacion_servicio": r["puntuacion_servicio"],
                 "puntuacion_comida": r["puntuacion_comida"]
